f80e3cf0f15e781c267889e2e399717a9b7c5f3a
Nicolas Lecrique <nicolaslecrique@hotmail.com>
2024-11-16 16:09:52 +0100

    manage fastAPI api settings

--- back/app/api.py
+++ back/app/api.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from fastapi import UploadFile
 from pydantic import BaseModel
 from uuid import UUID
+from app.settings import RouteSettings
 
 router: APIRouter = APIRouter(prefix="/api/v1")
 
@@ -20,19 +21,19 @@ class FileSnippetListResponse(BaseModel):
 
 
 @router.post("/files")
-async def create_file(file: UploadFile):
+async def create_file(file: UploadFile, settings: RouteSettings):
     pass
 
 @router.put("/files/{file_uuid}")
-async def update_file(file_id: str, file: UploadFile):
+async def update_file(file_id: str, file: UploadFile, settings: RouteSettings):
     pass
 
 @router.delete("/files/{file_uuid}")
-async def delete_file(file_uuid: UUID):
+async def delete_file(file_uuid: UUID, settings: RouteSettings):
     pass
 
 @router.get("/file_snippets")
-async def get_file_snippets()-> FileSnippetListResponse:
+async def get_file_snippets(settings: RouteSettings)-> FileSnippetListResponse:
     return FileSnippetListResponse(files=[])
 
 
@@ -48,5 +49,5 @@ class QueryRequest(BaseModel):
     question: str
 
 @router.post("/queries")
-async def create_query(query: QueryRequest) -> QueryResponse:
+async def create_query(query: QueryRequest, settings: RouteSettings) -> QueryResponse:
     return QueryResponse(answer="", references=[])
