d7ae4cc0008ed57216cf72a1af4f4a690f685bd6
Nicolas Lecrique <nicolaslecrique@hotmail.com>
2024-11-24 21:06:44 +0100

    lint

--- back/app/settings.py
+++ back/app/settings.py
@@ -1,5 +1,7 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
     seemantic_drive_root: str
