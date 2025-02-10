e375ccc4a0c531cbf21313cdd06b77618bc9166c
Nicolas Lecrique <nicolaslecrique@hotmail.com>
2025-02-10 21:52:57 +0100

    make embedding service work for big files

--- back/common/embedding_service.py
+++ back/common/embedding_service.py
@@ -12,6 +12,7 @@ class EmbeddingService:
 
     _url: Final[str] = "https://api.jina.ai/v1/embeddings"
     _headers: Final[dict[str, str]]
+    _max_chars: Final[int] = 15000 # heuristic because max is 8192 tokens
 
     def __init__(self, token: str) -> None:
         self._headers = {"Content-Type": "application/json", "Authorization": f"Bearer {token}"}
@@ -36,19 +37,36 @@ class EmbeddingService:
         embeddings = [Embedding(embedding=embedding["embedding"]) for embedding in json["data"]]
         return embeddings
 
+# TODO MANAGE TOO BUG DOCUMENT TOO LONG FOR LATE_CHUNKING
     async def embed_document(self, document: ParsedDocument, chunks: list[Chunk]) -> list[EmbeddedChunk]:
         """
         embed contiguous passages from a single document
         Nb: for now the number of tokens is limited to 8192 tokens ot it will crash.
         """
-        chunk_contents = [document[chunk] for chunk in chunks]
+        
+        chunk_groups: list[list[Chunk]] = []
+        current_group: list[Chunk] = []
+        start_index_current_group = 0
+        for chunk in chunks:
+            if chunk.end_index_in_doc - start_index_current_group > self._max_chars :
+                chunk_groups.append(current_group)
+                current_group = [chunk]
+                start_index_current_group = chunk.start_index_in_doc
+            else:
+                current_group.append(chunk)
+        # add the last group
+        if current_group:
+            chunk_groups.append(current_group)
 
-        embeddings = await self._embed("retrieval.passage", chunk_contents, late_chunking=True)
-        return [
-            EmbeddedChunk(chunk=chunk, embedding=embedding) for chunk, embedding in zip(chunks, embeddings, strict=True)
-        ]
+        results: list[EmbeddedChunk] = []
+        for group in chunk_groups:
+            chunk_contents = [document[chunk] for chunk in group]
+            embeddings = await self._embed("retrieval.passage", chunk_contents, late_chunking=False)
+            embedded_chunks = [EmbeddedChunk(chunk=chunk, embedding=embedding) for chunk, embedding in zip(group, embeddings, strict=True)]
+            results.extend(embedded_chunks)
 
-    # TODO(Nico): seems embedding does not return
+        return results
+
 
     async def embed_query(self, query: str) -> Embedding:
 
