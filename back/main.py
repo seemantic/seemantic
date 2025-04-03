c3a70be00b50124ee31472fa8e2b334c049fbc6a
Nicolas Lecrique <6695280+nicolaslecrique@users.noreply.github.com>
2025-04-03 22:49:58 +0200

    add cors config to server

--- back/main.py
+++ back/main.py
@@ -2,6 +2,7 @@ import logging
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.rest_api import router
 
@@ -9,7 +10,22 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 
 app = FastAPI()
 
+# Manage CORS
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1",
+    "http://127.0.0.1:3000",
+    "https://seemantic.io",  # Production domain
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
 
 
