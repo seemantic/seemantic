ccc1409688b8463cfe06923f282c16ecc5e58677
Nicolas Lecrique <6695280+nicolaslecrique@users.noreply.github.com>
2025-04-12 22:23:02 +0200

    add resizable to search screen

--- webapp/src/routes/_app/search.tsx
+++ webapp/src/routes/_app/search.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute, getRouteApi } from '@tanstack/react-router'
-
+import ChatCard from '../../components/biz/ChatCard' // Adjust the import path as needed
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable'
 
 type SearchParams = {
   q: string
@@ -18,9 +19,15 @@ function RouteComponent() {
   const searchParams = routeApi.useSearch()
   const { q } = searchParams
 
+  return (
 
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel>      <div className="flex justify-center items-center h-screen w-full">
+        <ChatCard />
+      </div></ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel>Two</ResizablePanel>
+    </ResizablePanelGroup>
 
-
-  
-  return <div>{q}</div>
+  )
 }
