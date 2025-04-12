30dff39a4d8a6d156db34868fa27e10c3c1414fe
Nicolas Lecrique <6695280+nicolaslecrique@users.noreply.github.com>
2025-04-12 11:50:55 +0200

    add search param to search route

--- webapp/src/routes/_app/search.tsx
+++ webapp/src/routes/_app/search.tsx
@@ -1,9 +1,21 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, getRouteApi } from '@tanstack/react-router'
+
+
+type SearchParams = {
+  q: string
+}
 
 export const Route = createFileRoute('/_app/search')({
   component: RouteComponent,
+  validateSearch: (search: Record<string, unknown>): SearchParams => {
+    return { q: search.q as string || '' }
+  },
 })
 
+const routeApi = getRouteApi('/_app/search')
+
 function RouteComponent() {
-  return <div>Hello "/search"!</div>
+  const searchParams = routeApi.useSearch()
+  const { q } = searchParams
+  return <div>{q}</div>
 }
