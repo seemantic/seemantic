147b686c498c82f04f86ffaf6a463386c93c14a4
Nicolas Lecrique <6695280+nicolaslecrique@users.noreply.github.com>
2025-04-12 11:18:08 +0200

    fix data load for search route

--- webapp/src/routes/_app/route.tsx
+++ webapp/src/routes/_app/route.tsx
@@ -1,9 +1,11 @@
 import LeftPanel from '@/components/biz/left-panel'
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { get_explorer } from '@/utils/api'
 import { createFileRoute, Outlet } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_app')({
   component: RouteComponent,
+  loader: async () => get_explorer()
 })
 
 function RouteComponent() {
