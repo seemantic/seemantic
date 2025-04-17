63bb799e54314a844fb732cb02b2e650245f2027
Nicolas Lecrique <6695280+nicolaslecrique@users.noreply.github.com>
2025-04-17 21:29:52 +0200

    kill  previous processes on start services scripts

--- start_backend_services.sh
+++ start_backend_services.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# Kill front and back services
+pkill -f vite || true
+pkill -f uvicorn || true
+# kill main_indexer.py
+ps ux | grep main_indexer | grep -v grep | awk '{print $2}' | xargs -r -I{} sh -c 'echo "Killing PID {}"; kill -9 {}'
+
 ./back/postgres_db/start_db.sh
 ./back/minio/start.sh
 cd back
