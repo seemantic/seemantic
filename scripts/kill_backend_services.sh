0065ac3be6d3b17fcf394568d1398581bf5fe59b
Nicolas Lecrique <6695280+nicolaslecrique@users.noreply.github.com>
2025-04-23 22:19:19 +0200

    move scripts into proper folder

--- scripts/kill_backend_services.sh
+++ scripts/kill_backend_services.sh
@@ -6,14 +6,3 @@ pkill -f vite || true
 pkill -f uvicorn || true
 # kill main_indexer.py
 ps ux | grep main_indexer | grep -v grep | awk '{print $2}' | xargs -r -I{} sh -c 'echo "Killing PID {}"; kill -9 {}'
-
-./back/postgres_db/start_db.sh
-./back/minio/start.sh
-cd back
-python -m main_indexer &
-uvicorn main:app &
-cd ..
-cd webapp
-npm run start &
-
-
