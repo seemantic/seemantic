40a12d8ec91d93dd72b4e3ada7edbd8e4d3e72fa
Nicolas Lecrique <6695280+nicolaslecrique@users.noreply.github.com>
2025-04-29 22:06:31 +0200

    extract rest api data in its own file

--- back/app/rest_api.py
+++ back/app/rest_api.py
@@ -1,7 +1,6 @@
 import asyncio
 import logging
 from collections.abc import AsyncGenerator
-from datetime import datetime
 from io import BytesIO
 from typing import Literal
 
@@ -11,6 +10,16 @@ from pydantic import BaseModel
 
 from app.app_services import DepDbService, DepGenerator, DepMinioService, DepSearchEngine
 from app.generator import ChatMessage
+from app.rest_api_data import (
+    ApiChatMessage,
+    ApiDocumentDelete,
+    ApiDocumentSnippet,
+    ApiExplorer,
+    ApiQuery,
+    ApiQueryResponseUpdate,
+    ApiSearchResult,
+    ApiSearchResultChunk,
+)
 from app.search_engine import SearchResult
 from app.settings import DepSettings
 from common.db_service import DbDocument, DbEventType, DbIndexedDocumentEvent
@@ -51,64 +60,6 @@ async def get_file(relative_path: str, minio_service: DepMinioService) -> Stream
 
     raise HTTPException(status_code=404, detail=f"File {relative_path} not found")
 
-
-class ApiDocumentSnippet(BaseModel):
-    uri: str  # relative path within source
-    status: Literal["pending", "indexing", "indexing_success", "indexing_error"]
-    error_status_message: str | None
-    last_indexing: datetime | None
-
-
-class ApiDocumentDelete(BaseModel):
-    uri: str
-
-
-class ApiExplorer(BaseModel):
-    documents: list[ApiDocumentSnippet]
-
-
-class ApiSearchResultChunk(BaseModel):
-    content: str
-    start_index_in_doc: int
-    end_index_in_doc: int
-
-
-class ApiSearchResult(BaseModel):
-    document_uri: str
-    chunks: list[ApiSearchResultChunk]
-
-class ApiChatMessage(BaseModel):
-    role: Literal["user", "assistant"]
-    content: str
-
-class ApiQueryResponseUpdate(BaseModel):
-    # if not None, it's the continuation of the answer
-    # if None, keep the previous answer.
-    delta_answer: str | None
-    # if None, keep the previous result.
-    # if not None, it replace the previous search result
-    search_result: list[ApiSearchResult] | None
-    # if None, keep the previous chat messages
-    # if not None, it replace the previous chat messages
-    chat_messages_exchanged: list[ApiChatMessage] | None
-
-
-class ApiQueryResponseMessage(BaseModel):
-    answer: str
-    search_result: list[ApiSearchResult]
-    chat_messages_exchanged: list[ApiChatMessage]
-
-class ApiQueryMessage(BaseModel):
-    content: str
-
-class ApiQueryReponsePair(BaseModel):
-    query: ApiQueryMessage
-    response: ApiQueryResponseMessage
-
-
-class ApiQuery(BaseModel):
-    query: ApiQueryMessage
-    previous_messages: list[ApiQueryReponsePair]
 
 
 def _to_api_doc(db_doc: DbDocument) -> ApiDocumentSnippet:
---
+++ back/app/rest_api_data.py
@@ -0,0 +1,64 @@
+
+from datetime import datetime
+from typing import Literal
+
+from pydantic import BaseModel
+
+
+class ApiDocumentSnippet(BaseModel):
+    uri: str  # relative path within source
+    status: Literal["pending", "indexing", "indexing_success", "indexing_error"]
+    error_status_message: str | None
+    last_indexing: datetime | None
+
+
+class ApiDocumentDelete(BaseModel):
+    uri: str
+
+
+class ApiExplorer(BaseModel):
+    documents: list[ApiDocumentSnippet]
+
+
+class ApiSearchResultChunk(BaseModel):
+    content: str
+    start_index_in_doc: int
+    end_index_in_doc: int
+
+
+class ApiSearchResult(BaseModel):
+    document_uri: str
+    chunks: list[ApiSearchResultChunk]
+
+class ApiChatMessage(BaseModel):
+    role: Literal["user", "assistant"]
+    content: str
+
+class ApiQueryResponseUpdate(BaseModel):
+    # if not None, it's the continuation of the answer
+    # if None, keep the previous answer.
+    delta_answer: str | None
+    # if None, keep the previous result.
+    # if not None, it replace the previous search result
+    search_result: list[ApiSearchResult] | None
+    # if None, keep the previous chat messages
+    # if not None, it replace the previous chat messages
+    chat_messages_exchanged: list[ApiChatMessage] | None
+
+
+class ApiQueryResponseMessage(BaseModel):
+    answer: str
+    search_result: list[ApiSearchResult]
+    chat_messages_exchanged: list[ApiChatMessage]
+
+class ApiQueryMessage(BaseModel):
+    content: str
+
+class ApiQueryReponsePair(BaseModel):
+    query: ApiQueryMessage
+    response: ApiQueryResponseMessage
+
+
+class ApiQuery(BaseModel):
+    query: ApiQueryMessage
+    previous_messages: list[ApiQueryReponsePair]
