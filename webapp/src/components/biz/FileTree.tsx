01c2bbaf37016412a3f707505ef3e09c1f385949
Nicolas Lecrique <6695280+nicolaslecrique@users.noreply.github.com>
2025-05-22 21:53:29 +0200

    add headless-tree and classname dep + draft tree

---
+++ webapp/src/components/biz/FileTree.tsx
@@ -0,0 +1,60 @@
+import {
+  hotkeysCoreFeature,
+  selectionFeature,
+  syncDataLoaderFeature,
+} from '@headless-tree/core'
+import { useTree } from '@headless-tree/react'
+import type { Meta } from '@storybook/react'
+import cn from 'classnames'
+
+const meta = {
+  title: 'React/General/Simple Example',
+  tags: ['homepage'],
+} satisfies Meta
+
+export default meta
+
+// story-start
+export const SimpleExample = () => {
+  const tree = useTree<string>({
+    initialState: { expandedItems: ['folder-1'] },
+    rootItemId: 'folder',
+    getItemName: (item) => item.getItemData(),
+    isItemFolder: (item) => !item.getItemData().endsWith('item'),
+    dataLoader: {
+      getItem: (itemId) => itemId,
+      getChildren: (itemId) => [
+        `${itemId}-1`,
+        `${itemId}-2`,
+        `${itemId}-3`,
+        `${itemId}-1item`,
+        `${itemId}-2item`,
+      ],
+    },
+    indent: 20,
+    features: [syncDataLoaderFeature, selectionFeature, hotkeysCoreFeature],
+  })
+
+  return (
+    <div {...tree.getContainerProps()} className="tree">
+      {tree.getItems().map((item) => (
+        <button
+          {...item.getProps()}
+          key={item.getId()}
+          style={{ paddingLeft: `${item.getItemMeta().level * 20}px` }}
+        >
+          <div
+            className={cn('treeitem', {
+              focused: item.isFocused(),
+              expanded: item.isExpanded(),
+              selected: item.isSelected(),
+              folder: item.isFolder(),
+            })}
+          >
+            {item.getItemName()}
+          </div>
+        </button>
+      ))}
+    </div>
+  )
+}
