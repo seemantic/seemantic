d5bc4c12ff23aad57273e3657c7f6abaccb87867
Nicolas Lecrique <6695280+nicolaslecrique@users.noreply.github.com>
2025-06-17 22:40:18 +0200

    manage case where convId is not found (when using link after refresh for example)
    because zustand does not store conv for now

--- webapp/src/components/biz/ConvPanel.tsx
+++ webapp/src/components/biz/ConvPanel.tsx
@@ -1,7 +1,7 @@
 import ChatCard from '@/components/biz/ChatCard' // Adjust the import path as needed
 import StreamedResponsePanel from '@/components/biz/StreamedResponsePanel'
 import { userConvStore } from '@/utils/conv_manager'
-import { useNavigate } from '@tanstack/react-router'
+import { Navigate, useNavigate } from '@tanstack/react-router'
 
 type ConvPanelProps = {
   convId: string
@@ -11,9 +11,18 @@ export default function ConvPanel(props: ConvPanelProps) {
   const { convId } = props
 
   const navigate = useNavigate()
-  const pairIds = userConvStore(
-    (state) => state.conversations[convId].queryResponsePairIds,
-  )
+  const pairIds = userConvStore((state) => {
+    // If the conversation does not exist, return an empty array
+    if (convId in state.conversations) {
+      return state.conversations[convId].queryResponsePairIds
+    } else {
+      return null
+    }
+  })
+
+  if (pairIds === null) {
+    return <Navigate to="/" replace={true} />
+  }
 
   const createConversation = userConvStore((state) => state.createConversation)
   const appendApiQueryResponsePair = userConvStore(
