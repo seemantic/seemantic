d627c82618929a4a9df85ccb6915578e4f4a100b
Nicolas Lecrique <6695280+nicolaslecrique@users.noreply.github.com>
2025-07-22 22:03:45 +0200

    add button to add folder

--- webapp/src/components/biz/LeftPanel.tsx
+++ webapp/src/components/biz/LeftPanel.tsx
@@ -2,11 +2,13 @@ import {
   Sidebar,
   SidebarContent,
   SidebarGroup,
+  SidebarGroupAction,
   SidebarGroupContent,
   SidebarGroupLabel,
   SidebarMenu,
 } from '@/shadcn/components/ui/sidebar'
 import { userConvStore } from '@/utils/conv_manager'
+import { FolderPlus } from 'lucide-react'
 import { FileTree } from './FileTree'
 
 export default function LeftPanel() {
@@ -19,6 +21,15 @@ export default function LeftPanel() {
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Seemantic</SidebarGroupLabel>
+          <SidebarGroupAction
+            title="New Folder"
+            onClick={() => {
+              // Dummy action
+              alert('New Folder action clicked')
+            }}
+          >
+            <FolderPlus /> <span className="sr-only">New Folder</span>
+          </SidebarGroupAction>
           <SidebarGroupContent>
             <SidebarMenu>
               <FileTree docs={docsArray} />
